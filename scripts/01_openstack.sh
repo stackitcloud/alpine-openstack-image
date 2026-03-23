2304c5d4ec5a045f4f59eb556c579a6d39ccd4a0
Maximilian Geberl <maximilian.geberl@digits.schwarz>
2026-03-23 15:29:32 +0100

    adopt to setup-cloud-init

--- scripts/01_openstack.sh
+++ scripts/01_openstack.sh
@@ -4,10 +4,8 @@ apk add --no-cache cloud-init
 apk add --no-cache acpi
 
 # Start Cloud-Init on Boot
-rc-update add cloud-init default
-rc-update add cloud-init-local default
-rc-update add cloud-config default
-rc-update add cloud-final default
+setup-cloud-init
+rc-update -u
 
 echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
 sed -i '/lock_passwd/c\    lock_passwd: False' /etc/cloud/cloud.cfg
